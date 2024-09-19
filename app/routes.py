54e73ca72a9648ae3b628ee5bf0832098fb390e5
Fabricio Taveira <fabricio.marques@trf1.jus.br>
2024-09-19 12:26:23 -0300

    Atualizando pagina de filmes

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ api_service = ApiFilmesService(api_key)
 @main.route('/')
 def index():
     return render_template('index.html')
-
+# ---------------------- ROTAS DE AUTENTICAÇÃO E CADASTRO --------------------------------
 @main.route('/entrar', methods=['POST'])
 def logar():
     if request.method == 'POST':
@@ -22,13 +22,39 @@ def logar():
     
     if usuario_existente and password == usuario_existente.password:
         nome = usuario_existente.username
-        id = usuario_existente.user_id
+        usuario_id = usuario_existente.user_id
+
+        # Armazena o usuário na sessão
+        session['usuario_id'] = usuario_id
         
-        return render_template('filmes.html', nome=nome, email=email, id=id)
+        return render_template('filmes.html', nome=nome, email=email, usuario_id=usuario_id)
     else:
         error = 'Email ou senha inválido'
         return render_template('index.html', error=error)
+
+@main.route('/cadastrar', methods=['POST'])
+def cadastrar():
+    if request.method == 'POST':
+        nome = request.form.get('nome_cadastro')
+        email = request.form.get('email_cadastro')
+        senha = request.form.get('senha_cadastro')
+        senhaConfirmada = request.form.get('confirm_senha')
+        
+        if senha == senhaConfirmada:
+            if session.query(User).filter_by(email=email).first():
+                error = 'Email já cadastrado'
+                return render_template('index.html', error=error)
+            
+            novo_usuario = User(username=nome, email=email, password=senha)
+            session.add(novo_usuario)
+            session.commit()
+            sucess = 'Usuário cadastrado com sucesso, faça seu login!'
+            return redirect(url_for('main.index', sucess=sucess))
+        else:
+            error = 'Senhas não conferem'
+            return redirect(url_for('main.index', error=error))
     
+# ---------------------- ROTAS FILMES --------------------------------
 @main.route('/pequisar', methods=['POST'])
 def pesquisar():
     nome_filme = request.form.get('nome_filme')
@@ -43,4 +69,30 @@ def pesquisar():
 def get_filme(id):
     filme = api_service.get_filme_by_id(id)
     return render_template('filme.html', filme=filme)
+
+# Rota de avaliação do filme
+@main.route('/filme/avaliar/<int:id>', methods=['POST'])
+def avaliar_filme(id):
+    if request.method == 'POST':
+        # Recebe a avaliação do formulário
+        rating = request.form.get('rating')
+        
+        # Recupera o user_id da sessão
+        usuario_id = session.get('usuario_id')
+        
+        # Identifica o id do filme
+        movie_id = id
+        
+        # Verifica se o usuário já fez a avaliação deste filme
+        avaliacao_existente = session.query(Ratings).filter_by(user_id=usuario_id, movie_id=movie_id).first()
+        
+        # Se não houver avaliação existente, adiciona uma nova
+        if not avaliacao_existente:
+            nova_avaliacao = Ratings(user_id=usuario_id, movie_id=movie_id, rating=rating)
+            session.add(nova_avaliacao)
+            session.commit()
+        
+        # Redireciona para a página do filme após a avaliação
+        return redirect(url_for('main.get_filme', id=movie_id))
+
     
